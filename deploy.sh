751839085ec68b514f65d820e30c2dcfb0847f55
Jerome Bidault <djedje.thai.ok@gmail.com>
2021-06-10 16:10:18 +0700

    commit to deploy

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,16 @@
-docker build -t djedjethai/kubclient -f ./client/Dockerfile ./client
-docker build -t djedjethai/kubserver -f ./server/Dockerfile ./server
-docker build -t djedjethai/kubworker -f ./worker/Dockerfile ./worker
-docker push djedjethai/kubclient
-docker push djedjethai/kubserver
-docker push djedjethai/kubworker
-kubctl apply -f k8s
+docker build -t djedjethai/kubclient:latest -t djedjethai/kubclient:$SHA -f ./client/Dockerfile ./client
+docker build -t djedjethai/kubserver:latest -t djedjethai/kubserver:$SHA -f ./server/Dockerfile ./server
+docker build -t djedjethai/kubworker:latest -t djedjethai/kubworker:$SHA -f ./worker/Dockerfile ./worker
 
+docker push djedjethai/kubclient:latest
+docker push djedjethai/kubserver:latest
+docker push djedjethai/kubworker:latest
+
+docker push djedjethai/kubclient:$SHA
+docker push djedjethai/kubserver:$SHA
+docker push djedjethai/kubworker:$SHA
+
+kubctl apply -f k8s
+kubectl set image deployments/server-deployment server=djedjethai/kubserver:$SHA
+kubectl set image deployments/client-deployment client=djedjethai/kubclient:$SHA
+kubectl set image deployments/worker-deployment worker=djedjethai/kubworker:$SHA
